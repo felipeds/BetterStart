1fd688099332c1b9089cb8c7778666fecb000bab
Felipe Demétrio de Souza <felipeds@gmail.com>
2018-01-09 21:03:02 -0500

    first release

---
+++ BetterStart/BetterStart/BetterStart.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using System;
+using System.Xml;
+
+namespace BetterStart
+{
+    //StartGear
+    [HarmonyPatch(typeof(StartGear), "AddAllToInventory")]
+    public class BetterStart
+    {
+
+        static void Postfix()
+        {
+            //this mod is for Survival only and should not affect story mode
+            if (GameManager.IsStoryMode()) return;
+
+
+            //loaditems
+
+            XmlDocument items = new XmlDocument();
+
+            items.Load("BetterStart.xml");
+
+            XmlNodeList list = items.GetElementsByTagName("item");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                GameManager.GetPlayerManagerComponent().InstantiateItemInPlayerInventory(list[i]["name"].InnerText, int.Parse(list[i]["quantity"].InnerText));
+            }
+         
+        }
+    }
+}
